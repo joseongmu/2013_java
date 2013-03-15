fdb6dd8022b1882aaa0069d6cd88aa13eb6fbfee
nnmm666 <nnmm666@naver.com>
2013-03-15 22:41:54 +0900

    chapter 6 complete

--- chapter9/src/Math/math.java
+++ chapter9/src/Math/math.java
@@ -1,4 +1,4 @@
-package Math;
+package math;
 
 import java.util.Scanner;
 
@@ -6,27 +6,23 @@ public class math {
 	public static void main(String args[])
 	{
 		Scanner scan = new Scanner(System.in);
-		System.out.println("1~100 의 숫자를 입력 하시오 ");
 		int game = (int)(Math.random()*100+1);
 		int challenge = 0;
 		
+		System.out.println("1~100 의 숫자를 입력 하시오 ");
 		while(true){
 			
 			int input = scan.nextInt();
 			challenge ++;
-			
-			if(input<game)
-			{
+			if(input<game){
 				System.out.println("더 큰수를 입력 하시오");
-			}
-			else if(input>game){
+			}else if(input>game){
 				System.out.println("작은 수를 입력 하시오");
-			}
-			else
-			{
+			}else{
 				System.out.println("정답은 "+input+"입니다. 도전횟수는"+challenge+"입니다");
 				break;
 			}	
 		}
+		scan.close();
 	}
 }
