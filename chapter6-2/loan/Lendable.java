2e9a24f104ffed2f27f69668c0d3cd302fcd9d32
joseongmu <rpspdlxm@naver.com>
2013-04-07 15:50:28 +0900

    old version delete

--- chapter6-2/loan/Lendable.java
+++
@@ -1,9 +0,0 @@
-package loan;
-
-public interface Lendable {
-	final static boolean STATE_BORROWED = false;
-	final static boolean STATE_NORMAL = true;
-	abstract void checkOut (String borrower, String date) throws Exception;
-	void checkIn();
-
-}
--- chapter6-2/src/Lendable.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface Lendable {
-	final static boolean STATE_BORROWED = false;
-	final static boolean STATE_NORMAL = true;
-	abstract void checkOut (String borrower, String date) throws Exception;
-	void checkIn();
-
-}
